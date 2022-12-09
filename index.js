ef0c513a9cf3b5e9d058d45f14500637208ccbb9
ness11 <habaines@gmail.com>
2022-12-09 14:28:32 +0100

    toggle working

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-$(document).ready(function($) {
+$(document).ready(function ($) {
 	// Votre code ici avec les appels à la fonction $()
-	$("#button").hover(function(){
-		$(this).css("background-color", "hsl(240, 100%, 5%)")
-			},function(){
-				$(this).css("background-color", "hsl(5, 85%, 63%)")
-			})
+	$('#button').hover(
+		function () {
+			$(this).css('background-color', 'hsl(240, 100%, 5%)');
+		},
+		function () {
+			$(this).css('background-color', 'hsl(5, 85%, 63%)');
+		},
+	);
 
-	$("[id=nav]").hover(function(){
-		$(this).css("color", "hsl(5, 85%, 63%")
-			},function(){
-				$(this).css("color", "hsl(236, 13%, 42%)")
-			})
+	$('[id=nav]').hover(
+		function () {
+			$(this).css('color', 'hsl(5, 85%, 63%');
+		},
+		function () {
+			$(this).css('color', 'hsl(236, 13%, 42%)');
+		},
+	);
 
-			$("[id=new]").hover(function(){
-				$(this).css("color", " hsl(35, 77%, 62%)")
-					},function(){
-						$(this).css("color", "hsl(36, 100%, 99%)")
-					})
-					$("[id=bottom-title]").hover(function(){
-						$(this).css("color", " hsl(5, 85%, 63%)")
-							},function(){
-								$(this).css("color", "hsl(240, 100%, 5%)")
-							})
+	$('[id=new]').hover(
+		function () {
+			$(this).css('color', ' hsl(35, 77%, 62%)');
+		},
+		function () {
+			$(this).css('color', 'hsl(36, 100%, 99%)');
+		},
+	);
+	$('[id=bottom-title]').hover(
+		function () {
+			$(this).css('color', ' hsl(5, 85%, 63%)');
+		},
+		function () {
+			$(this).css('color', 'hsl(240, 100%, 5%)');
+		},
+	);
+
+	if ($(window).width() <= 425) {
+		$('nav').hide();
+		$('.burgerMenu').on('click', function () {
+			$('nav').toggle(function () {});
+		});
+		$('.closeMenu').click(function () {
+			$('nav').toggle(function () {});
+		});
+	}
 });
-
-// $(document).ready(function ($) {
-// 	// Votre code ici avec les appels à la fonction $()
-// 	$('#button').hover(function () {
-// 		$(this).css('background-color', 'hsl(240, 100%, 5%)')
-// 	})
-// })
